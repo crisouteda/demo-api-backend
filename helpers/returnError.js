83a54a648ca11970261a8da6158154aa9687dbb3
cristina <cristina.outeda.rua@gmail.com>
2021-10-08 16:40:25 +0200

    added error handling

---
+++ helpers/returnError.js
@@ -0,0 +1,10 @@
+export const returnError = ({ statusCode, message }) => {
+  return {
+    statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({ message }),
+  };
+};
